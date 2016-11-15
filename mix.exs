ccb05ef56b11bae3bc5f6a7c46107fc7c68ea5f1
ShaunApps <shaun.apps@gmail.com>
2016-11-15 17:20:17 -0500

    converted responses and sorted data, tests for sorting

--- mix.exs
+++ mix.exs
@@ -28,7 +28,8 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.9.0"}
+      {:httpoison, "~> 0.9.0"},
+      {:poison,    "~> 1.5"}
     ]
   end
 end
